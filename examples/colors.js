59b7f7837f61ad271bc4c23f5839b13d1f2e9db2
mdemo <mengdesen09@qq.com>
2015-01-22 16:01:40 +0800

    bump 0.1.0

---
+++ examples/colors.js
@@ -0,0 +1,37 @@
+/**
+ * lark-log - example.js
+ * Copyright(c) 2014 mdemo(https://github.com/demohi)
+ * MIT Licensed
+ */
+
+'use strict';
+
+var log = require('./../lib/log');
+var colors = require('colors');
+
+var config = {
+    dateformat : "HH:MM:ss.L",
+    files: {
+        debug: {
+            path: 'debug.log',
+            options:{
+                encoding: 'utf8'
+            }
+        },
+        info: {
+            path: 'info.log'
+        },
+        error: {
+
+        }
+    },
+    filters : {
+        error : [ colors.red, colors.bold ]
+    }
+};
+var logger = log(config);
+
+logger.info('info' + Date.now());
+logger.debug('debug' + Date.now());
+logger.debug('debug' + Date.now());
+logger.error('error' + Date.now());
---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * lark-log - index.js
+ * Copyright(c) 2014 mdemo(https://github.com/demohi)
+ * MIT Licensed
+ */
+
+'use strict';
+
+module.exports = require('./lib/log');
+
+
+
+
