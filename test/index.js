399722dc696fe010910ccf5a1085df2a93904a76
mdemo <mengdesen09@qq.com>
2015-03-13 16:21:59 +0800

    add logic

--- test/index.js
+++ test/index.js
@@ -19,3 +19,15 @@ describe('sample use', function(){
     });
 
 });
+describe('logid', function(){
+    it('should use config logid', function(){
+        var logger = require('../')({logid:123});
+        var o = logger.info('hello');
+        assert.equal(o['logid'], 123);
+        assert.equal(o['message'], 'hello');
+        assert.equal(o['file'], 'index.js');
+        assert.equal(o['level'], 3);
+        assert.equal(o['title'], 'info');
+    });
+
+});
