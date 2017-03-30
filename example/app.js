38187a63c3361f66a58d47534854c27b40c1268c
Sun Haohao <viringbells@q..com>
2017-03-30 14:42:19 +0800

    fix

---
+++ example/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Logger  = require('..');
+
+const logger = new Logger();
+
+process.mainModule = module;
+
+logger.debug('Printed by debug');
+logger.log('Printed by log');
+logger.trace('Printed by trace');
+
+logger.notice('Printed by notice');
+logger.warn('Printed by warn');
+
+logger.error('Printed by error');
+logger.fatal('Printed by fatal');
+
+setTimeout(() => {
+    logger.warn('Printed by warn after 1s');
+    logger.close();
+}, 1000);
